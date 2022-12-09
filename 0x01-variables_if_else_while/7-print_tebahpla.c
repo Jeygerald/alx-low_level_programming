cad88c868b60e9ab9126ebe33b3f42602906e824
Jeygerald <jerrygerald.ij@gmail.com>
2022-12-09 05:57:04 -0800

    new task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints reversed alphabet.
- *   *
- *    * Return: Always 0 (Success)
+ *   main - Prints reversed alphabet.
+ *   
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
