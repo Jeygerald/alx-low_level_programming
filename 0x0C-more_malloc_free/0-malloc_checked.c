283d9f08bb08d53bc422027fdfdb1ccd4efc3d91
Jeygerald <jerrygerald.ij@gmail.com>
2023-01-12 15:07:53 -0800

    tag 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * * malloc_checked - allocates a memory to malloc
- * * @b: the int to be used
- * * Return: a pointer to the allocated memory, if fails, terminate with value 98
+ *  * malloc_checked - allocates a memory to malloc
+ *  * @b: the int to be used
+ ** Return: a pointer to the allocated memory,if fails, terminate with value 98
 */
 
 void *malloc_checked(unsigned int b)
