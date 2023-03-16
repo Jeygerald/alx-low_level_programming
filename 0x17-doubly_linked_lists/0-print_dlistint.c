f03efb7b49ff026bd21586eb4314b265b0eece2e
Jeygerald <jerrygerald.ij@gmail.com>
2023-03-16 15:34:19 -0700

    redo 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *  *  *print_dlistint - prints the contents of a doubly linked list
- *  *   *@h:list to print
- *  *    *Return:number of nodes
+ * *  *print_dlistint - prints the contents of a doubly linked list
+ * *   *@h:list to print
+ * *    *Return:number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
